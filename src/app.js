853107e79ed9725b0f58b6d2d0e0ff3fced12a89
Samaksh Garg <samakshg11@gmail.com>
2026-02-08 02:49:01 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -1,56 +1,37 @@
 const express = require("express");
-const { adminauth ,userauth} = require("./middlewares/auth");
+// const { adminauth ,userauth} = require("./middlewares/auth");
+
 const app =express();
-app.use("/admin",adminauth);
-app.use("/user",userauth);
-// app.use("/",(req,res)=>{
-//     res.send("Nothing happened");
-// });
-// app.get("/user",(req,res)=>{
-//     res.send("User getting data");
-// })
 
-// app.get("/user/:userId/:name/:password",(req,res)=>{
-//     console.log(req.params);
-//     // console.log(req.query);
-// // app.get(/.*fly$/,(req,res)=>{
-// // app.get("/ab\\?c", (req,res)=>{
-//     res.send({firstname:"Samaksh", lastname:"Garg"});
-// });
-
-//  app.use(
-//     "/user",
-//     (req,res,next)=>{
-//         console.log("This is the first function");
-//         next();
-//     },
-//     (req,res,next)=>{
-//         console.log("This is the second function");
-//         next();
-//     },
-//  )
+// app.use("/admin",adminauth);
+// app.use("/user",userauth);
 
 
-// app.get("/",(req,res,next)=>{
-//     console.log("handling response");
-//     next();
-// },
-// (req,res,next)=>{
-//     console.log("This is the next function");
-//     next();    
-// },
-// (req,res,next)=>{
-//     console.log("This is the next function");
-//     res.send("This is the third function");
-// });
-
-
-app.get("/admin/getdata",(req,res)=>{
-    res.send("This is the admin data");
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("Internal Server Error");
+    }
 });
+app.get("/user/getuserdata",(req,res) => {
+    // try{
+    throw new Error("This is an error in getuserdata route");
+    res.send("This is the user data");
+// }
+// catch(err){
+//     res.status(500).send("external Server Error");
+// }
+});
+// app.get("/admin/getdata",(req,res)=>{
+//     res.send("This is the admin data");
+// });
 
-app.post("/user/login",(req,res)=>{
-    res.send("The user has logged in");
+// app.post("/user/login",(req,res)=>{
+//     res.send("The user has logged in");
+// });
+app.use("/user",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("external  Server Error");
+    }
 });
 app.listen(3000,()=>{
     console.log("The server is successfully listening")
