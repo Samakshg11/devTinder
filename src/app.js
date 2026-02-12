029599efb82d74da16cd83326df29cc083124a90
Samaksh Garg <samakshg11@gmail.com>
2026-02-13 02:47:43 +0530

    GET apis

--- src/app.js
+++ src/app.js
@@ -14,7 +14,33 @@ app.post("/signup", async(req,res)=>{
         res.status(400).send("Error creating user"  + err.message);
     }
 });
+//GET user by email
 
+app.get("/user", async (req,res)=>{
+    const userEmail = req.body.emailId;
+try{
+    const users = await User.findOne({emailId: userEmail});
+    if(users.length === 0){
+        res.status(404).send("User not found");
+    }
+    else{
+        res.send(users);
+    }
+}
+catch(err){
+    res.status(400).send("Error fetching user");
+}
+});
+
+//GET users by feed API by getting all the users from the database
+app.get("/feed", async (req,res)=>{
+    try{
+    const users = await User.find({});
+    res.send(users);
+    }catch(err){
+        res.status(400).send("Error fetching user");
+    };
+});
 connectDB().then(()=>{
     console.log("Database connected successfully");
     app.listen(3000,()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
     lastName:{
         type:String,
     },
-    emailid:{
+    emailId:{
         type:String,
     },
     password:{
