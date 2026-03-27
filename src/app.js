115c41c1b0d60bf3b99c7596b2be9eb1d7c2d2e1
Samakshg11 <samakshgarg2005@gmail.com>
2026-03-28 02:13:21 +0530

    fix: use environment variable for server port instead of hardcoded value

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use("/",userRouter);
 
 connectDB().then(()=>{
     console.log("Database connected successfully");
-    app.listen(3000,()=>{
+    app.listen(process.env.PORT,()=>{
         console.log("The server is successfully listening");
     });
 })
